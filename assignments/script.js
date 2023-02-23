9d32bd824cd871dd49d771499a4f1ab3a09e87d0
NUZHAT KASAM  DABHOLKAR <93029178+NUZHATDABHOLKAR@users.noreply.github.com>
2023-02-23 18:28:46 +0530

    Add files via upload

---
+++ assignments/script.js
@@ -0,0 +1,17 @@
+function togglecontent() {
+    var content = document.getElementById("hamburger-content");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }                                                                             
+}
+
+const ctaButton = document.getElementById("cta-button");
+
+ctaButton.addEventListener("click", function() {
+  alert("Thank you for your interest!");
+});
+function search(){
+    alert("work is remaining of this annually seation please montly seation choose")
+  }
